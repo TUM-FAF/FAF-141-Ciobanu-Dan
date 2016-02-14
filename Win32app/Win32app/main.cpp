ed922742964cc832726fa0212ec335e6f18a57e3
Dan Ciobanu <danutsciobanu@gmail.com>
2016-02-14 02:24:35 +0200

    LAB1

---
+++ Win32app/Win32app/main.cpp
@@ -0,0 +1,96 @@
+#include<windows.h>
+
+LPCWSTR g_szClassName = L"mainClass";
+
+#define BUTTON_01 1
+#define BUTTON_02 2
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+	
+	switch (Msg)
+	{
+		case WM_CREATE:
+			{
+				LPCWSTR button01_ID = L"BUTTON";
+				LPCWSTR button01_text = L"DEFAULT";
+				HWND button01 = CreateWindowEx(NULL, button01_ID, button01_text, BS_DEFPUSHBUTTON | WS_VISIBLE | WS_BORDER | WS_CHILD, 5, 5, 85, 25, hwnd, (HMENU)BUTTON_01, NULL, NULL);
+	
+				LPCWSTR button02_ID = L"BUTTON";
+				LPCWSTR button02_text = L"CUSTOM";
+				HWND button02 = CreateWindowEx(NULL, button02_ID, button02_text, BS_DEFPUSHBUTTON | WS_VISIBLE | WS_BORDER | WS_CHILD, 5, 35, 85, 25, hwnd, (HMENU)BUTTON_02, NULL, NULL);
+	
+	
+			}
+			break;
+		case WM_COMMAND:
+			
+			{
+				switch (wParam) {
+			
+					case BUTTON_01:
+					
+						break;
+					case BUTTON_02:
+					
+						break;
+				}	
+			}
+			break;
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hwnd, Msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	
+	WNDCLASSEX wc;
+	MSG msg;
+	HWND hwnd;
+
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.style = 0;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.hIconSm = LoadIcon(NULL, IDC_ICON);
+	wc.hIcon = LoadIcon(NULL, IDC_ICON);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbClsExtra = 0;
+
+	if (!RegisterClassEx(&wc)) {
+		LPCWSTR Error01 = L"Could not register class";
+		LPCWSTR Error10_Caption = L"Error";
+		MessageBox(NULL, Error01, Error10_Caption, MB_OK | MB_ICONERROR);
+	}
+	
+	LPCWSTR WindowTitle = L"LAB 1";
+	
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, g_szClassName, WindowTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 300, NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL) {
+		LPCWSTR Error02 = L"Could not create window!";
+		LPCWSTR Error02_Caption = L"Error";
+		MessageBox(NULL, Error02, Error02_Caption, MB_OK | MB_ICONERROR);
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
